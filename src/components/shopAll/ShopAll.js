e33be71891c8a27c9957803eff94e7aab0136cea
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-04-17 17:50:58 +0500

    readme and product image added

--- src/components/shopAll/ShopAll.js
+++ src/components/shopAll/ShopAll.js
@@ -1,4 +1,10 @@
 import React from "react";
+import product1 from "../../images/product1.png";
+import product2 from "../../images/product2.png";
+import product3 from "../../images/product3.png";
+import product4 from "../../images/product4.png";
+import product5 from "../../images/product5.png";
+import product6 from "../../images/product6.png";
 
 const ShopAll = () => {
   let links = [
@@ -17,7 +23,7 @@ const ShopAll = () => {
   let products = [
     {
       id: 1,
-      url: "https://image-us.samsung.com/SamsungUS/home/home-appliances/ranges/gas/nsg6dg8500sraa/gallery/NSG6DG8500SR_01_SCOM.jpg?$product-card-small-jpg$",
+      url: product1,
       title:
         "Bespoke 6 cu. ft. Smart Slide-In Gas Range with Smart Oven Camera & Illuminated Precision Knobs",
       price: "Save $600",
@@ -26,7 +32,7 @@ const ShopAll = () => {
     },
     {
       id: 2,
-      url: "https://image-us.samsung.com/us/mobile-audio/galaxy-buds2-pro/gallery/graphite/Q4B4B2C-86-SM-R510_001_Front_Graphite_RGB-GalleryImage-1600x1200.jpg?$product-card-small-jpg$",
+      url: product2,
       title: "Galaxy Buds2 Pro",
       price: "Save up to $40.",
       form: "$189.99 before eligible trade-in.",
@@ -34,7 +40,7 @@ const ShopAll = () => {
     },
     {
       id: 3,
-      url: "https://image-us.samsung.com/SamsungUS/home/easy-asset/tbsr-9125/03212023/Q990C.jpg?$product-card-small-jpg$",
+      url: product3,
       title: "Q-series 11.1.4 ch. Wireless Dolby ATMOS Soundbar",
       price: "Save $500",
       form: "$1399.99",
@@ -42,7 +48,7 @@ const ShopAll = () => {
     },
     {
       id: 4,
-      url: "https://image-us.samsung.com/SamsungUS/home/home-appliances/refrigerators/4-door-flex/02082024/RF23DB990012_01_White_Glass_SCOM.jpg?$product-card-small-jpg$",
+      url: product4,
       title: "23 cu. ft. Bespoke 4-Door Flex Refrigerator with AI Family Hub",
       price: "Save $1200",
       form: "$3899",
@@ -50,7 +56,7 @@ const ShopAll = () => {
     },
     {
       id: 5,
-      url: "https://image-us.samsung.com/us/smartphones/galaxy-s24/all-gallery/01_E3_TitaniumGray_Lockup_1600x1200.jpg?$product-card-small-jpg$",
+      url: product5,
       title: "Galaxy S24 Ultra, 1TB (Unlocked)",
       price: "Save up to $150.",
       form: " From $1509.99 before eligible trade-in.",
@@ -58,7 +64,7 @@ const ShopAll = () => {
     },
     {
       id: 6,
-      url: "https://image-us.samsung.com/SamsungUS/home/home-appliances/washers/all-in-one-washer-dryer-combo/wd53dba900hza1/gallery/WD53DBA900HZA1_01_Dark_Steel_SCOM.jpg?$product-card-small-jpg$",
+      url: product6,
       title:
         "Bespoke 5.3 cu. ft. All-in-One AI Laundry Combo Ultra Capacity Washer and Ventless Heat Pump Dryer",
       price: "Save $1100",
@@ -66,6 +72,7 @@ const ShopAll = () => {
       btn: "Pre-order now",
     },
   ];
+  console.log(products);
   let mappingProducts = products?.map((el) => (
     <div key={el.id} className="card">
       <img src={el.url} alt={el.title} />
